ccc2bc6fb34abe7f137904b3b07ca0959f038d66
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2022-11-28 09:55:50 +0300

    fix

--- fastapi/src/models/film.py
+++ fastapi/src/models/film.py
@@ -1,17 +1,17 @@
 from typing import Optional, List
 
-from base_model import ESModel
-from person import ESPerson
+from models.base_model import ESModel
+from models.person import ESPerson
 
 
 class ESFilm(ESModel):
     id: str
-    imdb_rating: Optional[float]
-    genre: List[str] # [ESGenre] ?
+    # imdb_rating: Optional[float]
+    # genre: List[str]  # [ESGenre] ?
     title: str
     description: Optional[str]
-    director: List[str]
-    actors_names: List[str]
-    writers_names: List[str]
-    actors: List[ESPerson]
-    writers: List[ESPerson]
+    # director: List[str]
+    # actors_names: List[str]
+    # writers_names: List[str]
+    # actors: List[ESPerson]
+    # writers: List[ESPerson]
