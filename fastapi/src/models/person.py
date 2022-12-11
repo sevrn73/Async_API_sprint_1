d27d199cb7423ada1b2e689149e7bc82f6534d2c
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2022-12-11 21:33:39 +0300

    fix

--- fastapi/src/models/person.py
+++ fastapi/src/models/person.py
@@ -5,16 +5,3 @@ from models.base_model import ESModel
 class ESPerson(ESModel):
     id: str
     name: str
-    film_ids: List[str]
-
-
-class ESActor(ESPerson):
-    pass
-
-
-class ESDirector(ESPerson):
-    pass
-
-
-class ESWriter(ESPerson):
-    pass
