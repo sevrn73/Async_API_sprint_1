82cc7ba1d649515b1ffa7820932dabf625db39e3
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2022-12-11 21:21:58 +0300

    fix

--- fastapi/src/api/v1/films.py
+++ fastapi/src/api/v1/films.py
@@ -13,7 +13,6 @@ class Film(BaseModel):
     title: str
 
 
-# Внедряем FilmService с помощью Depends(get_film_service)
 @router.get('/{film_id}', response_model=Film)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
